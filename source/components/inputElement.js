3d7bfcf913f75d7be8b5b2bff58d3b42193f6ce5
udgir.pratik <udgir.pratik@gmail.com>
2017-07-22 14:19:04 -0700

    Changes

--- source/components/inputElement.js
+++ source/components/inputElement.js
@@ -2,8 +2,6 @@ import React from "react";
 const inputStyle = {
 		  color: 'black',
 		  border : 'solid green 2px',
-		  margin : '1px',
-		  float: 'right',
 		  background : '#ffffb3',
 		  height: '28px'
 		  
@@ -12,30 +10,73 @@ const inputStyle = {
 
 	const divStyle = {
 		  color: 'blue',
-		  width : '325px',
-		  border : 'solid red 1px',
-		  margin : '1px',
-			  height: '30px'
+		  width : '323px',
+			  height: '30px',
+			  float : 'right'
 		};
+	
+	const outerDiv = {
+			  color: 'blue',
+			  width : '400px',
+			  margin : '1px',
+				  height: '30px'
+			};
 
 	const labelStyle = {
 			  color: 'blue',
 			  margin : '2px',
-			  width : '200px',
+			  width : '200px'
 			  
 			};
+	
+	const requiredStyle = {
+			color : 'red',
+			textAlign : 'left'
+			  
+			
+	}
 
 export class InputElement extends React.Component {
-	
+		
+    constructor (props){
+    	super(props);
+    	this.state = {
+    		hasData : false
+    	}
+    	this.onUpdateField = this.onUpdateField.bind(this);
+    }	
+    
+    
+    onUpdateField(e){
+    	console.log(this , e);
+    	
+    	this.setState({
+    		hasData : true
+    	})
+    }
+    
+    renderRequiredMessage (){
+    	if(!this.state.hasData){
+    		return null;
+    	}
+    	return <span style={requiredStyle}>Required</span>;
+    }
+    
 	render (){
 
 		var fieldName = this.props.fieldName;
 		var labelName = this.props.labelName;
-		return <div style= {divStyle}>
-					<span style={labelStyle}>
-					{labelName} :
-					</span>
-					<input type="text" name="{fieldName}" style={inputStyle}/>
-			</div>;
+		return <div style={outerDiv}>
+		<span style={labelStyle}>
+		{labelName} :
+		
+				<div style= {divStyle}>
+					
+					<input ref="field" type="text" id="{fieldName}" style={inputStyle} onBlur={this.onUpdateField}/>
+					{this.renderRequiredMessage()}
+					</div>
+					</span>			
+					
+					</div>
 	}
 }
