acfab29f46de15da278bfd16205b8ef8e271dec3
Pratik Udgir <udgir.pratik@gmail.com>
2017-11-12 22:05:15 +0530

    Modifying files for sort feature

--- source/client.js
+++ source/client.js
@@ -12,20 +12,20 @@ sayHi();
 const heads = ['Password', 'Email', 'Text', 'Date', 'Time', 'Search'];
 const data = [{'password' : 'Password', 'email' : 'Email', 'text' : 'Text', 'date' : 'Date', 'time' : 'Time', 'search' : 'Search'},
 	{'password' : 'Password1', 'email' : 'Email1', 'text' : 'Text1', 'date' : 'Date1', 'time' : 'Time1', 'search' : 'Search1'},
+	{'password' : 'Password1', 'email' : 'Email211', 'text' : 'UV', 'date' : 'Date1', 'time' : 'Time1', 'search' : 'Search1'},
 {'password' : 'Password2', 'email' : 'Email2', 'text' : 'Text2', 'date' : 'Date2', 'time' : 'Time2', 'search' : 'Search2'}];
 
 ReactDOM.render(
 				<BoxDiv>
-			<InputElement fieldName="name" labelName="Name"/>
-			<InputElement fieldName="class" labelName="Class"/>
-			<InputElement fieldName="age" labelName="Age"/>
-			<InputElement fieldName="dob" labelName="DOB"/>
-			<InputWithImage fieldType="password" maxlength="10"/>
-			<InputWithImage fieldType="email" maxlength={10} required={true}/>
-			<InputWithImage fieldType="text" maxlength={10} required={true}/>
-			<InputWithImage fieldType="date" maxlength={10} required={true}/>
-			<InputWithImage fieldType="time" maxlength={10} required={true}/>
-			<InputWithImage fieldType="search" maxlength={10} required={true}/>
-			<Table heads={heads} data={data}/>
+				<InputElement fieldName="class" labelName="Class"/>
+					<InputElement fieldName="age" labelName="Age"/>
+					<InputElement fieldName="dob" labelName="DOB"/>
+					<InputWithImage fieldType="password" maxlength="10"/>
+					<InputWithImage fieldType="email" maxlength={10} required={true}/>
+					<InputWithImage fieldType="text" maxlength={10} required={true}/>
+					<InputWithImage fieldType="date" maxlength={10} required={true}/>
+					<InputWithImage fieldType="time" maxlength={10} required={true}/>
+					<InputWithImage fieldType="search" maxlength={10} required={true}/>
+			<Table heads={heads} data={data} defaultSort ="Password"/>
 			</BoxDiv>, 
 document.getElementById('expert'));
--- source/components/Table.js
+++ source/components/Table.js
@@ -5,15 +5,48 @@ export default class Table extends React.Component {
 	    super();
 	    this.state = {
 	    	filters : {},
-	    	records : []
+	    	records : [],
+	    	sort : {},
+	    	time : 1
 	    }
 	  }
 	
 	componentDidMount() {
 	    this.setState({ records : this.props.data });
+	    this.setState({
+    		column : this.props.defaultSort.toLowerCase(),
+    		order  : 'asc'
+    	});
 	  }
 	
 	sort (evt){
+		let key = evt.target.id.split('_')[0].toLowerCase();
+		let data = Object.assign({}, this.state.records);
+		let previousSort = Object.assign({}, this.state.sort);
+		let order =  this.state.sort.order;
+		let newOrder =  'asc';
+		if(previousSort.column === key){
+			if(order === 'asc'){
+				newOrder = 'desc';
+			} else
+			if(order === 'desc'){
+				newOrder = 'asc';
+			}
+		} else {
+			newOrder = 'asc';
+		}
+		let sortedRecords = Object.values(data);
+		sortedRecords.sort(function(a, b) {
+	        var x = a[key]; var y = b[key];
+	        if(newOrder === 'asc'){
+	        	console.log('asc');
+	        	return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+	        } else {
+	        	console.log('desc');
+	        	return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+	        }
+	    });
+		this.setState({sort : {column : key , order : newOrder}, records : sortedRecords, time : 2})
 	}
 	
 	filter (evt){
@@ -57,8 +90,18 @@ export default class Table extends React.Component {
 						function(name, index){
 							return <th key={index} style={headStyle}>
 									{name}
-                    					<img id={name+'_up'} src="./images/upArrow.png" onClick={this.sort.bind(this)}/>
-                    					<img id={name+'_down'} src="./images/downArrow.png" onClick={this.sort.bind(this)}/>
+									{this.state.sort.column !== name.toLowerCase() ? 
+											<span><img id={name+'_up'} src="./images/upArrow.png" onClick={this.sort.bind(this)}/>
+									
+											<img id={name+'_down'} src="./images/downArrow.png" onClick={this.sort.bind(this)}/></span> : null}
+									{console.log(this.state.sort.column === name.toLowerCase())}
+									{console.log(this.state.sort.order === 'asc')}
+									{console.log(this.state.time !== 1)}
+									{this.state.sort.column === name.toLowerCase() && this.state.sort.order === 'asc' && this.state.time !== 1 ?
+                    					<img id={name+'_up'} src="./images/upArrow.png" onClick={this.sort.bind(this)}/> : null}
+									{this.state.sort.column === name.toLowerCase() && this.state.sort.order === 'desc' && this.state.time !== 1 ?
+										<img id={name+'_down'} src="./images/downArrow.png" onClick={this.sort.bind(this)}/> : null}
+									
                     					<input id={name+'_filter'}type="search" style={{marginTop : 2}} onBlur={this.filter.bind(this)}/>
                     				</th>;
 						}, this)
